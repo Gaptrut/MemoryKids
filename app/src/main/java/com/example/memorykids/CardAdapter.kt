1f782bf61420f2e2eb92f51c1740015f3d3179d7
Cecilia Ringkvist <ceciliaringkvist@Cecilias-MacBook-Pro-2.local>
2023-12-07 23:03:51 +0100

    ljudochbilder

--- app/src/main/java/com/example/memorykids/CardAdapter.kt
+++ app/src/main/java/com/example/memorykids/CardAdapter.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 
 import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -97,10 +98,13 @@ class CardAdapter(private var cardList: List<Card>, private val context: Context
 
 
                     if (matchedPairsCount == totalPairs) {
+                        val mediaPlayer =
+                            MediaPlayer.create(holder.cardImage.context, R.raw.yipee)
+                        mediaPlayer.start()
                         // Visa en toast-notis
                         Toast.makeText(
                             holder.cardImage.context,
-                            "Game over! Starting new game...",
+                            "Seger! 🎉En gång till??\uD83D\uDE0A",
                             Toast.LENGTH_SHORT
                         ).show()
 
--- app/src/main/java/com/example/memorykids/NewGameActivity.kt
+++ app/src/main/java/com/example/memorykids/NewGameActivity.kt
@@ -1,6 +1,7 @@
 package com.example.memorykids
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,12 +10,16 @@ import android.widget.Toast
 
 
 class NewGameActivity : AppCompatActivity() {
+
+    private var mediaPlayer: MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_game)
 
-
-
+        mediaPlayer = MediaPlayer.create(this, R.raw.baaaa)
+        val maxVolume = 100  //Procentandel. 100 = 100%, 50 = 50%, osv.
+        mediaPlayer?.setVolume(maxVolume.toFloat(), maxVolume.toFloat())
+        mediaPlayer?.start()
 
 
         val easyButton = findViewById<Button>(R.id.buttonEasy)
@@ -26,6 +31,7 @@ class NewGameActivity : AppCompatActivity() {
 
         val mediumButton = findViewById<Button>(R.id.buttonMedium)
         mediumButton.setOnClickListener {
+            mediaPlayer?.stop()
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtra("difficulty", "Mellan")
             startActivity(intent)
@@ -33,6 +39,7 @@ class NewGameActivity : AppCompatActivity() {
 
         val hardButton = findViewById<Button>(R.id.buttonHard)
         hardButton.setOnClickListener {
+            mediaPlayer?.stop()
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtra("difficulty", "Svår")
             startActivity(intent)
@@ -40,8 +47,17 @@ class NewGameActivity : AppCompatActivity() {
 
         val exitButton: ImageButton = findViewById(R.id.exitButton)
         exitButton.setOnClickListener {
-            Toast.makeText(this, "Hejdå! 👋😊", Toast.LENGTH_SHORT).show()
-            finish()
+            mediaPlayer?.stop()
+            Toast.makeText(this, "Hejdå! 👋😊",
+                Toast.LENGTH_SHORT).show()
+            val homeIntent = Intent(Intent.ACTION_MAIN)
+            homeIntent.addCategory(Intent.CATEGORY_HOME)
+            startActivity(homeIntent)
         }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        // Släpp mediaPlayer när aktiviteten förstörs
+        mediaPlayer?.release()
     }
 }
